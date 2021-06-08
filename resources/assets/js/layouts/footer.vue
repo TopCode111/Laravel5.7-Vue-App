9ece3391b593593aa63c6e3d4578c1468aaeab92
topcode111 <aldahir.martinez9197@gmail.com>
2021-06-08 14:01:15 +0800

    deploy app

---
+++ resources/assets/js/layouts/footer.vue
@@ -0,0 +1,12 @@
+<template>
+	<footer class="footer">
+        Developed by Takahashi
+    </footer>
+</template>
+
+<script>
+    export default {
+        mounted() {
+        }
+    }
+</script>
---
+++ resources/assets/js/layouts/guest-footer.vue
@@ -0,0 +1,10 @@
+<template>
+	<div>
+		<p class="text-center"><small>Developed by Takahashi</small></p>
+	</div>
+</template>
+
+
+<script>
+    export default { }
+</script
